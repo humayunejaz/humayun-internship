ead3a216506d0b5a99cdb01d04b8763750a7da44
Humayun Ejaz <humayunejazm@gmail.com>
2026-08-03 16:09:47 -0400

    new userfeature

--- src/components/collection/CollectionGrid.jsx
+++ src/components/collection/CollectionGrid.jsx
@@ -64,33 +64,13 @@ function ItemCard({ item, linkPrefix = "/item" }) {
 export function SkeletonCard() {
   return (
     <div className="collection">
-      <Skeleton width="100%" height="200px" borderRadius="0px" />
+      <Skeleton width="100%" height="200px" borderRadius="12px" />
       <div className="collection__info">
-        <div className="collection__stats">
-          <div className="collection__stat">
-            <Skeleton width="60px" height="0.85rem" borderRadius="4px" />
-            <Skeleton width="90px" height="1rem" borderRadius="4px" />
-          </div>
-          <div className="collection__stat">
-            <Skeleton width="80px" height="0.85rem" borderRadius="4px" />
-            <Skeleton width="90px" height="1rem" borderRadius="4px" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export function SkeletonItemCard() {
-  return (
-    <div className="item">
-      <figure className="item__img__wrapper">
-        <Skeleton width="100%" height="100%" borderRadius="0px" />
-      </figure>
-      <div className="item__details">
         <Skeleton width="60%" height="1rem" borderRadius="4px" />
-        <Skeleton width="40%" height="1rem" borderRadius="4px" />
-        <Skeleton width="50%" height="0.8rem" borderRadius="4px" />
+        <div className="collection__stats">
+          <Skeleton width="80px" height="1rem" borderRadius="4px" />
+          <Skeleton width="80px" height="1rem" borderRadius="4px" />
+        </div>
       </div>
     </div>
   );
@@ -160,7 +140,7 @@ export default function CollectionGrid({
           {loading
             ? new Array(6).fill(0).map((_, i) => (
                 <SwiperSlide key={i} className="collection-column">
-                  {isItemCard ? <SkeletonItemCard /> : <SkeletonCard />}
+                  <SkeletonCard />
                 </SwiperSlide>
               ))
             : collections.map((item) => (
@@ -184,9 +164,9 @@ export default function CollectionGrid({
     <>
       <div className="collections__body">
         {loading
-          ? new Array(12).fill(0).map((_, i) => (
+          ? new Array(6).fill(0).map((_, i) => (
               <div key={i} className="collection-column">
-                {isItemCard ? <SkeletonItemCard /> : <SkeletonCard />}
+                <SkeletonCard />
               </div>
             ))
           : collections.slice(0, visible).map((item) => (
@@ -202,7 +182,7 @@ export default function CollectionGrid({
               </div>
             ))}
       </div>
-      {loadMore && (loading || visible < collections.length) && (
+      {loadMore && !loading && visible < collections.length && (
         <button
           className="collections-page__button"
           onClick={() => setVisible((prev) => prev + 6)}
